38af0864de5c64eb0be18c073d3f143b35a8a83b
printempw <h@prinzeugen.net>
2016-02-03 13:27:13 +0800

    add last-modified support

--- get.php
+++ get.php
@@ -3,7 +3,7 @@
  * @Author: prpr
  * @Date:   2016-02-02 20:56:42
  * @Last Modified by:   prpr
- * @Last Modified time: 2016-02-02 21:33:12
+ * @Last Modified time: 2016-02-03 13:26:30
  */
 
 $dir = dirname(__FILE__);
@@ -12,13 +12,16 @@ require "$dir/config.php";
 
 if (isset($_GET['type']) && isset($_GET['uname'])) {
     $user = new user($_GET['uname']);
-
     if (!$user->is_registered) utils::raise(1, 'Non-existent user.');
 
-    if ($_GET['type'] == "skin") {
-        echo $user->getBinaryTexture('skin');
-    } else if ($_GET['type'] == "cape") {
-        echo $user->getBinaryTexture('cape');
+    $if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) : null;
+
+    if ($_GET['type'] == "skin" || $_GET['type'] == "cape") {
+        if ($if_modified_since >= $user->getLastModified()) {
+            header('HTTP/1.0 304 Not Modified');
+        } else {
+            echo $user->getBinaryTexture($_GET['type']);
+        }
     } else if ($_GET['type'] == "json") {
         echo $user->getJsonProfile();
     } else {
