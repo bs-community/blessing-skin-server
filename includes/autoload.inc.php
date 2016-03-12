706a4cb8c1268ca227e84809287b7465876fc157
printempw <h@prinzeugen.net>
2016-03-12 17:21:30 +0800

    fixed bug

--- includes/autoload.inc.php
+++ includes/autoload.inc.php
@@ -2,8 +2,8 @@
 /**
  * @Author: prpr
  * @Date:   2016-02-02 21:17:59
- * @Last Modified by:   prpr
- * @Last Modified time: 2016-02-04 18:34:34
+ * @Last Modified by:   printempw
+ * @Last Modified time: 2016-03-12 17:20:54
  */
 
 function __autoload($classname) {
@@ -11,4 +11,6 @@ function __autoload($classname) {
     $filename = "$dir/includes/".$classname.".class.php";
     include_once($filename);
 }
+if (!file_exists($dir.'/config.php'))
+    Utils::raise(-1, '未找到 `config.php`，请确认配置文件是否存在。');
 require "$dir/config.php";
