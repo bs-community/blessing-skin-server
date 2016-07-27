e3dccf23d6c291154b35e75f9399d9c37d151f6d
printempw <h@prinzeugen.net>
2016-07-27 22:31:49 +0800

    enhance performance at homepage

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,7 +9,22 @@ class HomeController extends BaseController
 
     public function index()
     {
-        echo \View::make('index')->with('user', (isset($_SESSION['email']) ? new User($_SESSION['email']) : null));
+        if (isset($_COOKIE['email']) && isset($_COOKIE['token'])) {
+            $user = new User($_COOKIE['email']);
+
+            if ($_COOKIE['token'] == $user->getToken() && $user->getPermission() != "-1") {
+                $_SESSION['email'] = $_COOKIE['email'];
+                $_SESSION['token'] = $_COOKIE['token'];
+            } else {
+                // delete cookies
+                setcookie("email", "", time() - 3600, '/');
+                setcookie("token", "", time() - 3600, '/');
+            }
+        }
+
+        $user = isset($_SESSION['email']) ? new User($_SESSION['email']) : null;
+
+        echo \View::make('index')->with('user', $user);
     }
 
 }
