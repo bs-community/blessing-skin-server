c6cedc8ca346fd51b4cfd45500a9cc62ff3c374b
printempw <h@prinzeugen.net>
2016-08-11 18:11:20 +0800

    update to v3.0.2

--- setup/update_scripts/update-3.0.1-to-3.0.2.php
+++ setup/update_scripts/update-3.0.1-to-3.0.2.php
@@ -3,7 +3,18 @@
  * @Author: printempw
  * @Date:   2016-08-11 13:08:13
  * @Last Modified by:   printempw
- * @Last Modified time: 2016-08-11 13:15:35
+ * @Last Modified time: 2016-08-11 13:25:23
  */
 
-echo "shit";
+$options = [
+    'avatar_query_string' => '0',
+    'version'             => '',
+    'check_update'        => '1',
+    'update_source'       => 'github'
+];
+
+foreach ($options as $key => $value) {
+    Option::add($key, $value);
+}
+
+Option::set('version', '3.0.2');
