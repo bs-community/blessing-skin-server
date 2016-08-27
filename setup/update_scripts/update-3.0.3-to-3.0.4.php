e4bd07f86a9d98681d2a48646977cc9cbd91a645
printempw <h@prinzeugen.net>
2016-08-27 18:23:05 +0800

    add update script for v3.0.4

---
+++ setup/update_scripts/update-3.0.3-to-3.0.4.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @Author: printempw
+ * @Date:   2016-08-27 18:21:04
+ * @Last Modified by:   printempw
+ * @Last Modified time: 2016-08-27 18:21:15
+ */
+
+if (Option::get('update_source') == "github") {
+    Option::set('update_source', 'nyavm');
+}
+
+Option::set('version', '3.0.4');
