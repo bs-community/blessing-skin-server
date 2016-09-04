1fa51927bbb9f6ebe3c4d58b65571fba11aded24
printempw <h@prinzeugen.net>
2016-09-04 15:47:51 +0800

    use composer to load helpers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use View;
 use Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,9 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::addExtension('tpl', 'blade');
-
-        require_once BASE_DIR."/app/helpers.php";
+        View::addExtension('tpl', 'blade');
 
         Validator::extend('username', function($attribute, $value, $parameters, $validator) {
             return preg_match("/^([A-Za-z0-9\x{4e00}-\x{9fa5}_]+)$/u", $value);
