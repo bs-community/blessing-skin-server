02fe0c67f1422bf6cdf887c89079b5bbd946a1e7
printempw <h@prinzeugen.net>
2016-10-16 22:29:49 +0800

    update sources for updating

--- config/update.php
+++ config/update.php
@@ -10,9 +10,9 @@
 
 return array(
     'nyavm' => [
-        'name'        => 'NyaVM',
-        'update_url'  => 'http://work.prinzeugen.net/update.json',
-        'description' => '感谢 <a href="https://www.nyavm.com/">NyaVM</a> 提供的 Anycast CDN，国内外主机都可获得不错的速度。NyaVM 主机，百兆 CN2 只需 $3.99 月付，稳定快速大带宽。'
+        'name'        => 'LizCat',
+        'update_url'  => 'http://cdn.prinzeugen.net/update.json',
+        'description' => '感谢 <a href="https://www.nyavm.com/">NyaVM</a> 提供的 Anycast CDN，国内外主机都可获得不错的速度。'
     ],
     'little_service' => [
         'name'        => 'LittleService-COS',
@@ -22,6 +22,6 @@ return array(
     'local' => [
         'name'        => 'LocalHost',
         'update_url'  => 'http://127.0.0.1/test/update.json',
-        'description' => '开发用，请勿选择（炸了别怪我）'
+        'description' => '本地调试用，请勿选择（炸了别怪我）'
     ]
 );
