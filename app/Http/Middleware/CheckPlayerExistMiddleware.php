a6751473f64ab912595435ce70f40441a73b98a9
printempw <h@prinzeugen.net>
2016-11-06 10:58:01 +0800

    event CheckPlayerExists now returns bool value

--- app/Http/Middleware/CheckPlayerExistMiddleware.php
+++ app/Http/Middleware/CheckPlayerExistMiddleware.php
@@ -18,21 +18,24 @@ class CheckPlayerExistMiddleware
 
         $player_name = urldecode($matches[1]);
 
-        Event::fire(new CheckPlayerExists($player_name));
+        $responses = Event::fire(new CheckPlayerExists($player_name));
 
-        if (Player::where('player_name', $player_name)->get()->isEmpty()) {
-            if (option('return_200_when_notfound') == "1") {
-                return json([
-                    'player_name' => $player_name,
-                    'errno'       => 404,
-                    'msg'         => 'Player Not Found.'
-                ])->header('Cache-Control', 'public, max-age='.option('cache_expire_time'));
-            } else {
-                abort(404, trans('general.unexistent-player'));
-            }
+        foreach ($responses as $r) {
+            if ($r) return $next($request);
         }
 
-        return $next($request);
+        if (!Player::where('player_name', $player_name)->get()->isEmpty())
+            return $next($request);
+
+        if (option('return_200_when_notfound') == "1") {
+            return json([
+                'player_name' => $player_name,
+                'errno'       => 404,
+                'msg'         => 'Player Not Found.'
+            ])->header('Cache-Control', 'public, max-age='.option('cache_expire_time'));
+        } else {
+            abort(404, trans('general.unexistent-player'));
+        }
 
     }
 }
