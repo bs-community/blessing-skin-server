6834b6894820f46ae1072a9b97f54184d6be7297
printempw <h@prinzeugen.net>
2016-11-21 22:04:10 +0800

    add runtime check

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
  * @author   printempw <h@prinzeugen.net>
  */
 
+// runtime check
+if (version_compare(PHP_VERSION, '5.5.9', '<')) {
+    exit('[Error] Blessing Skin Server needs PHP version >= 5.5.9, you are now using '.PHP_VERSION);
+}
+
+if (!class_exists('PDO')) {
+    exit('[Error] You have not installed the PDO extension');
+}
+
+if (!function_exists('openssl_encrypt')) {
+    exit('[Error] You have not installed the OpenSSL extension');
+}
+
 require __DIR__.'/bootstrap/autoload.php';
 
 $app = require_once __DIR__.'/bootstrap/app.php';
