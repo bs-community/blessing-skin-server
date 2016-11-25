28ea2b2858a460b707f8fe444fcb5da971b13ed3
printempw <h@prinzeugen.net>
2016-11-25 22:34:41 +0800

    separate v2 data migrations as a plugin

--- config/menu.php
+++ config/menu.php
@@ -23,7 +23,6 @@ $menu['admin'] = array(
     ['title' => 'general.customize',     'link' => 'admin/customize',  'icon' => 'fa-paint-brush'],
     ['title' => 'general.score-options', 'link' => 'admin/score',      'icon' => 'fa-credit-card'],
     ['title' => 'general.options',       'link' => 'admin/options',    'icon' => 'fa-cog'],
-    ['title' => 'general.import-v2',     'link' => 'setup/migrations', 'icon' => 'fa-undo'],
     ['title' => 'general.check-update',  'link' => 'admin/update',     'icon' => 'fa-arrow-up']
 );
 
