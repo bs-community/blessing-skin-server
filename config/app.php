91ff0fd2a7334d5756d713b21e93c950aa282163
printempw <h@prinzeugen.net>
2016-12-10 18:55:08 +0800

    start using laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -213,6 +213,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Str' => Illuminate\Support\Str::class,
         'Arr' => Illuminate\Support\Arr::class,
 
