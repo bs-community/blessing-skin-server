cf0b1e3cacbacdec0f0c84c1675851805c016b5c
printempw <h@prinzeugen.net>
2017-08-05 14:15:26 +0800

    Update comment format

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 @ini_set('display_errors', 'on');
 
-// check PHP version
+// Check PHP version
 if (version_compare(PHP_VERSION, '5.5.9', '<')) {
     exit(
         '[Error] Blessing Skin Server needs PHP version >= 5.5.9, you are now using '.PHP_VERSION.'<br>'.
@@ -19,11 +19,11 @@ if (version_compare(PHP_VERSION, '5.5.9', '<')) {
 
 require __DIR__.'/bootstrap/autoload.php';
 
-// check the runtime environment
+// Check the runtime environment
 runtime_check(array(
     'extensions' => array('pdo_mysql', 'openssl', 'gd', 'mbstring', 'tokenizer'),
     'write_permission' => array('storage', 'resources', 'plugins')
 ));
 
-// handle the request
+// Process the request
 require __DIR__.'/bootstrap/kernel.php';
