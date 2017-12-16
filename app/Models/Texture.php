38b630777694e4ac01eb508f864fbd8aa2466240
Pig Fang <g-plane@hotmail.com>
2017-12-16 21:03:57 +0800

    Remove unused code

--- app/Models/Texture.php
+++ app/Models/Texture.php
@@ -9,16 +9,6 @@ class Texture extends Model
     public $primaryKey = 'tid';
     public $timestamps = false;
 
-    public static function checkTextureOccupied($tid)
-    {
-        $skin_type_map = ["steve", "alex", "cape"];
-        for ($i = 0; $i <= 2; $i++) {
-            if (Player::where('tid_'.$skin_type_map[$i], $tid)->count() > 0)
-                return true;
-        }
-        return false;
-    }
-
     public function setPrivacy($public)
     {
         $this->public = $public ? "1" : "0";
