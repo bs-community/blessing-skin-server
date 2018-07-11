85baa91a4a17dec98160a716423fb62b3601bf40
Pig Fang <g-plane@hotmail.com>
2018-07-11 16:45:07 +0800

    fix reading closet

--- app/Models/Closet.php
+++ app/Models/Closet.php
@@ -41,7 +41,7 @@ class Closet
         $this->db  = DB::table('closets');
 
         // Create a new closet if not exists
-        if ($this->db->where('uid', $uid)->isEmpty()) {
+        if ($this->db->where('uid', $uid)->count() == 0) {
             $this->db->insert([
                 'uid'      => $uid,
                 'textures' => '[]'
