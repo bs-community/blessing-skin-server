97d2d9cac271a02d0e177b876835e85bae5afc04
Pig Fang <g-plane@hotmail.com>
2018-07-13 22:56:22 +0800

    upgrade laravel to 5.5 and other deps

---
+++ config/yaml-translation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Array handling
+    |--------------------------------------------------------------------------
+    |
+    | Shall this package use the array_dot schema of Laravel?
+    | If true, this will flatten all array contents into a string.
+    */
+
+    'dot_syntax' => true,
+];
