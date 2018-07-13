b74cc0dd59f603b1e6cef6f296dbaacb0e83d0e3
Pig Fang <g-plane@hotmail.com>
2018-07-13 16:48:15 +0800

    fix tests of AdminController

--- tests/AdminControllerTest.php
+++ tests/AdminControllerTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class AdminControllerTest extends TestCase
+class AdminControllerTest extends BrowserKitTestCase
 {
     use DatabaseTransactions;
 
