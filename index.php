f3ef8d30f155f0314113e72236fc95b00c04f10b
Pig Fang <g-plane@hotmail.com>
2018-07-14 14:53:41 +0800

    update php version requirement

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 @ini_set('display_errors', 'on');
 
 // Check PHP version
-if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+if (version_compare(PHP_VERSION, '7.1.3', '<')) {
     header('Content-Type: text/html; charset=UTF-8');
     exit(
-        '[Error] Blessing Skin requires PHP version >= 7.0.0, you are now using '.PHP_VERSION.'<br>'.
-        '[错误] 你的 PHP 版本过低（'.PHP_VERSION.'），Blessing Skin 要求至少为 7.0.0'
+        '[Error] Blessing Skin requires PHP version >= 7.1.3, you are now using '.PHP_VERSION.'<br>'.
+        '[错误] 你的 PHP 版本过低（'.PHP_VERSION.'），Blessing Skin 要求至少为 7.1.3'
     );
 }
 
