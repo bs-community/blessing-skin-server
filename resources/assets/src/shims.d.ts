a8a3ba4ec959872454733ec8c722876b0db97789
Pig Fang <g-plane@hotmail.com>
2018-07-17 09:39:14 +0800

    add types shims

---
+++ resources/assets/src/shims.d.ts
@@ -0,0 +1,10 @@
+declare var blessing: {
+  base_url: string
+  debug: boolean
+  env: string
+  fallback_locale: string
+  locale: string
+  site_name: string
+  timezone: string
+  version: string
+}
