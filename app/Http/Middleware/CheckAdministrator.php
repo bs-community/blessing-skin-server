8f3088bcb35e541d659feb324f6bf3e52c7163c2
printempw <h@prinzeugen.net>
2018-07-22 14:08:25 +0800

    Update middleware CheckAdministrator

--- app/Http/Middleware/CheckAdministrator.php
+++ app/Http/Middleware/CheckAdministrator.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Middleware;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class CheckAdministrator
 {
     public function handle($request, \Closure $next)
     {
         $result = (new CheckAuthenticated)->handle($request, $next, true);
 
-        if ($result instanceof \Illuminate\Http\RedirectResponse) {
+        if ($result instanceof Response) {
             return $result;
         }
 
