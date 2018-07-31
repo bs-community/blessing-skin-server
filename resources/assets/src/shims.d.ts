72946e2474643f43b9b5b0ff20aecccfd8ab747a
Pig Fang <g-plane@hotmail.com>
2018-07-31 22:20:42 +0800

    Add more TypeScript declarations

--- resources/assets/src/shims.d.ts
+++ resources/assets/src/shims.d.ts
@@ -1,11 +1,21 @@
-declare var blessing: {
-  base_url: string
-  debug: boolean
-  env: string
-  fallback_locale: string
-  locale: string
-  site_name: string
-  timezone: string
-  version: string
-  route: string
+import Vue from 'vue'
+
+declare global {
+  var blessing: {
+    base_url: string
+    debug: boolean
+    env: string
+    fallback_locale: string
+    locale: string
+    site_name: string
+    timezone: string
+    version: string
+    route: string
+  }
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $t(key: string, parameters?: object): string
+  }
 }
