cb7f4d9806a7e36b0f9c1cfad92516c2130dab61
printempw <h@prinzeugen.net>
2018-08-07 18:15:39 +0800

    Add certificate and user agent config for http requests

--- config/secure.php
+++ config/secure.php
@@ -3,12 +3,26 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Configuration about security
+    | Security Configuration
     |--------------------------------------------------------------------------
     |
     | Load them from env to config, preventing cache problems
     |
     */
     'cipher' => menv('PWD_METHOD', 'SALTED2MD5'),
-    'salt'   => menv('SALT', '')
+    'salt'   => menv('SALT', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | SSL Certificates
+    |--------------------------------------------------------------------------
+    |
+    | Describes the SSL certificate verification behavior of all Guzzle requests.
+    | By default, we use the CA bundle provided by Mozilla.
+    |
+    | See: http://docs.guzzlephp.org/en/stable/request-options.html#verify
+    |
+    */
+    'certificates' => menv('SSL_CERT', storage_path('patches/ca-bundle.crt')),
+    'user_agent' => menv('USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'),
 ];
