1a84ea1506ee8869981c52fb97e0827167e26a22
printempw <h@prinzeugen.net>
2018-08-15 15:43:19 +0800

    Make notice box on player page always expanded

--- resources/views/user/player.blade.php
+++ resources/views/user/player.blade.php
@@ -63,12 +63,9 @@
                     </div>
                 </div>
 
-                <div class="box box-default collapsed-box">
+                <div class="box box-default">
                     <div class="box-header with-border">
                         <h3 class="box-title">{{ trans('general.notice') }}</h3>
-                        <div class="box-tools pull-right">
-                            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
-                        </div><!-- /.box-tools -->
                     </div><!-- /.box-header -->
                     <div class="box-body">
                         <p>{{ trans('user.player.login-notice') }}</p>
