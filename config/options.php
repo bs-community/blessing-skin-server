7e050ed632d8e809e8732519eff7f940d8d95ea1
printempw <h@prinzeugen.net>
2018-08-16 15:15:46 +0800

    Update initializing copyright_text option

--- config/options.php
+++ config/options.php
@@ -30,7 +30,7 @@ return [
     'score_per_player'          => '100',
     'sign_after_zero'           => 'false',
     'version'                   => '',
-    'copyright_text'            => '<strong>Copyright &copy; '.getdate()['year'].' <a href="{site_url}">{site_name}</a>.</strong> All rights reserved.',
+    'copyright_text'            => '<strong>Copyright &copy; {year} <a href="{site_url}">{site_name}</a>.</strong> All rights reserved.',
     'auto_del_invalid_texture'  => 'false',
     'allow_downloading_texture' => 'true',
     'texture_name_regexp'       => '',
