7103d52a6ed27d875806888781ecd8449e812c3d
Pig Fang <g-plane@hotmail.com>
2018-08-21 09:13:43 +0800

    Clean up

--- resources/assets/src/js/index.js
+++ resources/assets/src/js/index.js
@@ -3,3 +3,12 @@ import './i18n';
 import './net';
 import './layout';
 import './logout';
+
+console.log(
+    `%c Blessing Skin %c v${blessing.version} %c Made with %c<3%c by printempw.%c https://blessing.studio`,
+    'color:#fadfa3;background:#030307;padding:5px 0;margin:10px 0;',
+    'background:#fadfa3;padding:5px 0;',
+    'font-style:italic;',
+    'color:red;',
+    'font-style:italic;', ''
+);
