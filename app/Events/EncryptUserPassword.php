cfbfe60f898ec763b53073cdd0e93745bc4db204
timgiroux <40814794+timgiroux@users.noreply.github.com>
2018-10-09 04:00:26 -0700

    change variable name from rawPasswd to rawPassword (#128)

--- app/Events/EncryptUserPassword.php
+++ app/Events/EncryptUserPassword.php
@@ -8,18 +8,18 @@ class EncryptUserPassword extends Event
 {
     public $user;
 
-    public $rawPasswd;
+    public $rawPassword;
 
     /**
      * Create a new event instance.
      *
-     * @param  string $rawPasswd
+     * @param  string $rawPassword
      * @param  User   $user
      * @return void
      */
-    public function __construct($rawPasswd, User $user)
+    public function __construct($rawPassword, User $user)
     {
-        $this->rawPasswd = $rawPasswd;
+        $this->rawPassword = $rawPassword;
         $this->user = $user;
     }
 }
