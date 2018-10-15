14368e851c6d72cf650de27ca9e0b93d2571c9ad
Pig Fang <g-plane@hotmail.com>
2018-10-15 20:36:03 +0800

    Upgrade deps

--- scripts/fix-vue-good-table.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-const manifest = JSON.parse(fs.readFileSync('./node_modules/vue-good-table/package.json', 'utf-8'));
-delete manifest.sideEffects;
-fs.writeFileSync('./node_modules/vue-good-table/package.json', JSON.stringify(manifest, null, 2));
