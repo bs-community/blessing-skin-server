b6cf6d21a17db6f354fe3e4c1c9c5a0b92aed152
Pig Fang <g-plane@hotmail.com>
2019-03-26 10:42:25 +0800

    Rework admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,49 +20,58 @@
             <div class="col-md-6">
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="info-box">
-                            <a href="{{ url('admin/users') }}">
-                                <span class="info-box-icon bg-aqua"><i class="fas fa-users"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text">@lang('admin.index.total-users')</span>
-                                    <span class="info-box-number">{{ App\Models\User::count() }}</span>
-                                </div><!-- /.info-box-content -->
+                        <div class="small-box bg-aqua">
+                            <div class="inner">
+                                <h3>{{ App\Models\User::count() }}</h3>
+                                <p>@lang('admin.index.total-users')</p>
+                            </div>
+                            <div class="icon"><i class="fas fa-users"></i></div>
+                            <a href="{{ url('admin/users') }}" class="small-box-footer">
+                                @lang('general.user-manage') <i class="fa fa-arrow-circle-right"></i>
                             </a>
-                        </div><!-- /.info-box -->
+                        </div>
                     </div>
 
                     <div class="col-md-6">
-                        <div class="info-box">
-                            <a href="{{ url('admin/players') }}">
-                                <span class="info-box-icon bg-green"><i class="fas fa-gamepad"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text">@lang('admin.index.total-players')</span>
-                                    <span class="info-box-number">{{ App\Models\Player::count() }}</span>
-                                </div><!-- /.info-box-content -->
+                        <div class="small-box bg-green">
+                            <div class="inner">
+                                <h3>{{ App\Models\Player::count() }}</h3>
+                                <p>@lang('admin.index.total-players')</p>
+                            </div>
+                            <div class="icon"><i class="fas fa-gamepad"></i></div>
+                            <a href="{{ url('admin/players') }}" class="small-box-footer">
+                                @lang('general.player-manage') <i class="fa fa-arrow-circle-right"></i>
                             </a>
-                        </div><!-- /.info-box -->
+                        </div>
                     </div>
                 </div>
 
-                <div class="info-box">
-                    <span class="info-box-icon bg-aqua" style="background-color: #605ca8 !important;"><i class="far fa-file"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">@lang('admin.index.total-textures')</span>
-                        <span class="info-box-number">{{ App\Models\Texture::count() }}</span>
-                    </div><!-- /.info-box-content -->
-                </div><!-- /.info-box -->
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="small-box bg-purple">
+                            <div class="inner">
+                                <h3>{{ App\Models\Texture::count() }}</h3>
+                                <p>@lang('admin.index.total-textures')</p>
+                            </div>
+                            <div class="icon"><i class="fas fa-file"></i></div>
+                        </div>
+                    </div>
 
-                <div class="info-box">
-                    <span class="info-box-icon bg-yellow"><i class="far fa-hdd"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">@lang('admin.index.disk-usage')</span>
-                        @php
-                            $size = DB::table('textures')->sum('size') ?: 0;
-                        @endphp
-                        <span class="info-box-number">{{ $size > 1024 ? round($size / 1024, 1)."MB" : $size."KB" }}</span>
-                    </div><!-- /.info-box-content -->
-                </div><!-- /.info-box -->
+                    <div class="col-md-6">
+                        <div class="small-box bg-yellow">
+                            <div class="inner">
+                                @php
+                                    $size = DB::table('textures')->sum('size') ?: 0;
+                                @endphp
+                                <h3>{{ $size > 1024 ? round($size / 1024, 1).'MB' : $size.'KB' }}</h3>
+                                <p>@lang('admin.index.disk-usage')</p>
+                            </div>
+                            <div class="icon"><i class="fas fa-hdd"></i></div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
             <div class="col-md-6">
                 <div class="box box-primary">
                     <div class="box-header with-border">
