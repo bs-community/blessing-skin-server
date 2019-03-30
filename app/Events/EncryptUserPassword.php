e2b4153dab2001cb16a2781bb250ea4a7daee53f
Pig Fang <g-plane@hotmail.com>
2019-03-30 13:08:13 +0800

    Simplify property name
    Ref: #14

--- app/Events/EncryptUserPassword.php
+++ app/Events/EncryptUserPassword.php
@@ -8,18 +8,18 @@ class EncryptUserPassword extends Event
 {
     public $user;
 
-    public $rawPasswd;
+    public $raw;
 
     /**
      * Create a new event instance.
      *
-     * @param  string $rawPasswd
+     * @param  string $raw The raw password before encrypted.
      * @param  User   $user
      * @return void
      */
-    public function __construct($rawPasswd, User $user)
+    public function __construct($raw, User $user)
     {
-        $this->rawPasswd = $rawPasswd;
+        $this->raw = $raw;
         $this->user = $user;
     }
 }
