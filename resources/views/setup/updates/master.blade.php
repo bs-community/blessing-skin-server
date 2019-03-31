444dee3f143b29577bf6e86b9d79767a74901991
Pig Fang <g-plane@hotmail.com>
2019-03-31 14:47:48 +0800

    Fix loading CSS on update page

--- resources/views/setup/updates/master.blade.php
+++ resources/views/setup/updates/master.blade.php
@@ -5,15 +5,16 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="robots" content="noindex,nofollow" />
     <title>@lang('setup.updates.master.title')</title>
-    <link rel="shortcut icon" href="../resources/assets/dist/images/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="../resources/assets/dist/css/install.css">
+    <link rel="shortcut icon" href="{{ webpack_assets('favicon.ico') }}">
+    <link rel="stylesheet" type="text/css" href="{{ webpack_assets('setup.css') }}">
 </head>
 
 <body class="container">
-<p id="logo"><a href="https://github.com/printempw/blessing-skin-server" tabindex="-1">Blessing Skin Server</a></p>
+<p id="logo">
+    <a href="https://github.com/printempw/blessing-skin-server" tabindex="-1">Blessing Skin Server</a>
+</p>
 
 @yield('content')
 
 </body>
-
 </html>
