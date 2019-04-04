eca20dec5062ad9ee27d21231a329035df6a766b
Pig Fang <g-plane@hotmail.com>
2019-04-04 22:14:45 +0800

    Bump version to 4.0.0-beta.2

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     | Version of Blessing Skin Server.
     |
     */
-    'version' => '4.0.0-beta.1',
+    'version' => '4.0.0-beta.2',
 
     /*
     |--------------------------------------------------------------------------
