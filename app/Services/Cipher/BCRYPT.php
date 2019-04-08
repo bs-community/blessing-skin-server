5e42368d69b1725fd31577acdce099763430d681
Pig Fang <g-plane@hotmail.com>
2019-04-08 21:53:00 +0800

    New hash algorithm: Bcrypt

--- app/Services/Cipher/BCRYPT.php
+++ app/Services/Cipher/BCRYPT.php
@@ -2,11 +2,11 @@
 
 namespace App\Services\Cipher;
 
-class PHP_PASSWORD_HASH extends BaseCipher
+class BCRYPT extends BaseCipher
 {
     public function hash($value, $salt = '')
     {
-        return password_hash($value, PASSWORD_DEFAULT);
+        return password_hash($value, PASSWORD_BCRYPT);
     }
 
     public function verify($password, $hash, $salt = '')
