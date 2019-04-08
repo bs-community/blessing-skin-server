d55387a0d340e66d732a4bbcedeb6ebd448e0f2a
Pig Fang <g-plane@hotmail.com>
2019-04-08 15:22:39 +0800

    Add missing URL [skip ci]

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@ set -e
 mkdir dist
 cd dist
 cp ../blessing-skin-server-$RELEASE_TAG.zip blessing-skin-server-$RELEASE_TAG.zip
-echo "{\"spec\":1,\"latest\":\"$RELEASE_TAG\",\"url\":\"\"}" > update.json
+echo "{\"spec\":1,\"latest\":\"$RELEASE_TAG\",\"url\":\"https://dev.azure.com/blessing-skin/51010f6d-9f99-40f1-a262-0a67f788df32/_apis/git/repositories/a9ff8df7-6dc3-4ff8-bb22-4871d3a43936/Items?path=%2Fblessing-skin-server-$RELEASE_TAG.zip\"}" > update.json
 git init
 git add .
 git commit -m "Publish"
