b5ee28e60a60deb414937f1525d157cd0f6c9b3c
Pig Fang <g-plane@hotmail.com>
2019-05-01 19:04:14 +0800

    Display site name on error page

--- resources/views/errors/general.blade.php
+++ resources/views/errors/general.blade.php
@@ -4,12 +4,12 @@
     <meta name="viewport" content="width=device-width" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="robots" content="noindex,nofollow" />
-    <title>@yield('title') - Blessing Skin Server</title>
+    <title>@yield('title') - {{ option_localized('site_name') }}</title>
     <link rel="stylesheet" href="{{ webpack_assets('setup.css') }}">
 </head>
 
 <body class="container">
-<p id="logo"><a href="https://github.com/bs-community/blessing-skin-server" tabindex="-1">Blessing Skin Server</a></p>
+<p id="logo"><a href="https://github.com/bs-community/blessing-skin-server" tabindex="-1">{{ option_localized('site_name') }}</a></p>
 
 @yield('content')
 
