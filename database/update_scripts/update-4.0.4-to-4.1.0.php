22d49f972d26da22d4212d13f4162c821dd56f1d
Pig Fang <g-plane@hotmail.com>
2019-05-01 17:08:55 +0800

    Fix loading commands of laravel/passport

--- database/update_scripts/update-4.0.4-to-4.1.0.php
+++ database/update_scripts/update-4.0.4-to-4.1.0.php
@@ -2,6 +2,10 @@
 
 use Artisan;
 
+\Illuminate\Console\Application::starting(function ($artisan) {
+    $artisan->resolveCommands([\Laravel\Passport\Console\KeysCommand::class]);
+});
+
 Artisan::call('jwt:secret', ['--no-interaction' => true]);
 Artisan::call('migrate', ['--force' => true]);
 Artisan::call('passport:keys', ['--no-interaction' => true]);
