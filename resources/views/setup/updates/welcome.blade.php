2b575d7a8fb9057589f6acdaccb509d08c2ca2b8
Pig Fang <g-plane@hotmail.com>
2019-05-01 16:40:18 +0800

    Fix update page

--- resources/views/setup/updates/welcome.blade.php
+++ resources/views/setup/updates/welcome.blade.php
@@ -8,7 +8,8 @@
 
 <p>{!! nl2br(trans('setup.updates.welcome.text', ['version' => config('app.version')])) !!}</p>
 
-<form method="post" action="" novalidate="novalidate">
+<form method="post" action="{{ url('/setup/update') }}" novalidate="novalidate">
+    @csrf
     <p class="step">
         <input type="submit" name="submit" class="button button-large" value="@lang('setup.updates.welcome.button')"  />
     </p>
