b4e23ce79f805da6211acb98d174fb79b4e31bc8
Pig Fang <g-plane@hotmail.com>
2019-05-03 08:32:09 +0800

    Add message for 4.1.0 update script

--- database/update_scripts/update-4.0.4-to-4.1.0.php
+++ database/update_scripts/update-4.0.4-to-4.1.0.php
@@ -6,6 +6,17 @@ use Artisan;
     $artisan->resolveCommands([\Laravel\Passport\Console\KeysCommand::class]);
 });
 
+$tips = [];
+
 Artisan::call('jwt:secret', ['--no-interaction' => true]);
 Artisan::call('migrate', ['--force' => true]);
-Artisan::call('passport:keys', ['--no-interaction' => true]);
+try {
+    Artisan::call('passport:keys', ['--no-interaction' => true]);
+} catch (\Exception $e) {
+    $tips[] = nl2br(implode("\n", [
+        '您需要打开终端或 PowerShell 来执行这条命令：<code>php artisan passport:keys</code>',
+        'You need to open terminal or PowerShell and execute: <code>php artisan passport:keys</code>'
+    ]));
+}
+
+return $tips;
