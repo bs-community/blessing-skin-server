0105c6b016fe5087efec62cedf3faddfacea0e8f
Pig Fang <g-plane@hotmail.com>
2019-07-05 23:25:15 +0800

    Use `sync` as default queue driver

--- database/update_scripts/update-4.3.6-to-4.3.7.php
+++ database/update_scripts/update-4.3.6-to-4.3.7.php
@@ -4,7 +4,4 @@ return [
     '请您手动打开终端或 PowerShell 执行以下命令以完成升级：',
     'Please open terminal or PowerShell to complete upgrade:',
     '<code>php artisan migrate --force</code>',
-    '',
-    '然后修改 .env 文件，将 <code>QUEUE_DRIVER</code> 的值改为 <code>database</code>。（使用 Redis 的用户请修改为 <code>redis</code>）',
-    'Then, please edit your ".env" file. Change the value of <code>QUEUE_DRIVER</code> from <code>sync</code> to <code>database</code>. Redis users please change it to <code>redis</code>.',
 ];
