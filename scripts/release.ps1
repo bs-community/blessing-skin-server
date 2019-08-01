c5da87b681fcfb7957952223cef5d34cc5edbc6e
Pig Fang <g-plane@hotmail.com>
2019-08-01 14:38:26 +0800

    Update release script

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -3,19 +3,23 @@ $last = $manifest.latest
 $current = (Get-Content package.json | ConvertFrom-Json).version
 
 if ($last -eq $current) {
+    Write-Host "Latest version is $last. No need to publish." -ForegroundColor Green -BackgroundColor DarkMagenta
     exit
 }
 
 Install-Module PSGitHub -Force
+Write-Host "'PSGitHub' has been installed." -ForegroundColor Green
 
 # Install dependencies
 composer install --no-dev
 Remove-Item vendor/bin -Recurse -Force
 yarn
 yarn build
+Write-Host "Dependencies have been installed." -ForegroundColor Green
 
 $zip = "blessing-skin-server-$current.zip"
 zip -9 -r $zip app bootstrap config database plugins public resources/lang resources/views resources/misc routes storage vendor .env.example artisan LICENSE README.md README_EN.md
+Write-Host "Zip archive is created." -ForegroundColor Green
 
 New-Item dist -ItemType Directory
 Set-Location dist
@@ -24,6 +28,7 @@ Copy-Item -Path "../$zip" -Destination $zip
 $manifest.latest = $current
 $manifest.url = $manifest.url.Replace($last, $current)
 ConvertTo-Json $manifest | Out-File -FilePath update_2.json
+Write-Host "Update source is prepared." -ForegroundColor Green
 
 $azureToken = $env:AZURE_TOKEN
 git config --global user.email 'g-plane@hotmail.com'
@@ -32,8 +37,15 @@ git init
 git add .
 git commit -m "Publish"
 git push -f "https://anything:$azureToken@dev.azure.com/blessing-skin/Blessing%20Skin%20Server/_git/Blessing%20Skin%20Server" master
+Write-Host "Update source is pushed to Azure Repos." -ForegroundColor Green
 
 $githubToken = $env:GITHUB_TOKEN | ConvertTo-SecureString -AsPlainText -Force
 $enChangelog = Get-Content "../resources/misc/changelogs/en/$current.md"
 $changelog = "`n---`n" + $enChangelog
-New-GitHubRelease -Token $githubToken -Owner 'bs-community' -Repository 'blessing-skin-server' -TagName $current -ReleaseNote $changelog
+$release = New-GitHubRelease -Token $githubToken -Owner 'bs-community' -Repository 'blessing-skin-server' -TagName $current -ReleaseNote $changelog
+try {
+    New-GitHubReleaseAsset -Token $githubToken -Owner 'bs-community' -Repository 'blessing-skin-server'  -ReleaseId $release.Id -Path $zip
+} catch {
+    # Do nothing.
+}
+Write-Host "New version $current is published!" -ForegroundColor Green -BackgroundColor DarkMagenta
