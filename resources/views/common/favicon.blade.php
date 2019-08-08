aa635c16af9619085d6f4d1f2c3e40b5c9f07870
Pig Fang <g-plane@hotmail.com>
2019-08-08 22:27:16 +0800

    Fix importing `Illuminate\Support\Str`

--- resources/views/common/favicon.blade.php
+++ resources/views/common/favicon.blade.php
@@ -1,5 +1,5 @@
 @php
-    $faviconUrl = Str::startsWith($url = (option('favicon_url') ?: config('options.favicon_url')), 'http') ? $url : url($url);
+    $faviconUrl = \Illuminate\Support\Str::startsWith($url = (option('favicon_url') ?: config('options.favicon_url')), 'http') ? $url : url($url);
 @endphp
 <link rel="shortcut icon" href="{{ $faviconUrl }}">
 <link rel="icon" type="image/png" href="{{ $faviconUrl }}" sizes="192x192">
