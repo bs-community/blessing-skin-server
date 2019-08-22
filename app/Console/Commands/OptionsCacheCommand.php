197c3d833306247c0d9049b93fc11d222cf3c4cb
Pig Fang <g-plane@hotmail.com>
2019-08-22 08:50:59 +0800

    Tweak `options:cache` command

--- app/Console/Commands/OptionsCacheCommand.php
+++ app/Console/Commands/OptionsCacheCommand.php
@@ -12,25 +12,11 @@ class OptionsCacheCommand extends Command
 
     protected $description = 'Cache Blessing Skin options';
 
-    /** @var Filesystem */
-    protected $filesystem;
-
-    /** @var Option */
-    protected $options;
-
-    public function __construct(Filesystem $filesystem, Option $options)
+    public function handle(Filesystem $filesystem, Option $options)
     {
-        parent::__construct();
-
-        $this->filesystem = $filesystem;
-        $this->options = $options;
-    }
-
-    public function handle()
-    {
-        $content = var_export($this->options->all(), true);
+        $content = var_export($options->all(), true);
         $content = '<?php'.PHP_EOL.'return '.$content.';';
-        $this->filesystem->put(storage_path('options/cache.php'), $content);
+        $filesystem->put(storage_path('options/cache.php'), $content);
         $this->info('Options cached successfully.');
     }
 }
--- tests/CommandsTest/OptionsCacheCommandTest.php
+++ tests/CommandsTest/OptionsCacheCommandTest.php
@@ -9,16 +9,17 @@ class OptionsCacheCommandTest extends TestCase
 {
     public function testRun()
     {
-        /*$this->mock(Filesystem::class, function ($mock) {
+        $this->mock(Filesystem::class, function ($mock) {
             $mock->shouldReceive('put')
-                /*->withArgs(function ($path, $content) {
+                ->withArgs(function ($path, $content) {
                     $this->assertEquals(storage_path('options/cache.php'), $path);
-                    $this->assertTrue(Str::startsWith($content), '<?php');
+                    $this->assertTrue(Str::startsWith($content, '<?php'.PHP_EOL.'return'));
+                    $this->assertTrue(Str::endsWith($content, ';'));
 
                     return true;
                 })
                 ->once();
-        });*/
+        });
 
         $this->artisan('options:cache')->expectsOutput('Options cached successfully.');
     }
