1c5cb7fdba7dad79ee724255150256cc9700cdf9
Asnxthaony <asnxthaony@outlook.com>
2020-01-03 19:03:22 +0800

    Change default update source [skip ci]

--- config/app.php
+++ config/app.php
@@ -22,7 +22,7 @@ return [
     'update_source' => env(
         'UPDATE_SOURCE',
         'https://dev.azure.com/blessing-skin/51010f6d-9f99-40f1-a262-0a67f788df32/_apis/git/'.
-        'repositories/a9ff8df7-6dc3-4ff8-bb22-4871d3a43936/Items?path=%2Fupdate_2.json'
+        'repositories/a9ff8df7-6dc3-4ff8-bb22-4871d3a43936/Items?path=%2Fupdate_preview.json'
     ),
 
     /*
