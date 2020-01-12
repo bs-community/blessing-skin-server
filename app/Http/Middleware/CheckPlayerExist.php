18073094497d22a3f706c50819d32a27fab6e0b5
Pig Fang <g-plane@hotmail.com>
2020-01-12 17:14:23 +0800

    remove unused code

--- app/Http/Middleware/CheckPlayerExist.php
+++ app/Http/Middleware/CheckPlayerExist.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Events\CheckPlayerExists;
 use App\Models\Player;
-use Event;
 use Illuminate\Support\Arr;
 
 class CheckPlayerExist
@@ -12,38 +10,10 @@ class CheckPlayerExist
     public function handle($request, \Closure $next)
     {
         $pid = Arr::get($request->route()->parameters, 'pid') ?? $request->input('pid');
-        if (!$request->isMethod('get') && !is_null($pid)) {
-            if (is_null(Player::find($pid))) {
-                return json(trans('general.unexistent-player'), 1);
-            } else {
-                return $next($request);
-            }
+        if (!$request->isMethod('get') && !is_null($pid) && is_null(Player::find($pid))) {
+            return json(trans('general.unexistent-player'), 1);
         }
 
-        preg_match('/\/([^\/]*)\.json/', $request->getUri(), $matches);
-        $player_name = urldecode($matches[1]);
-        $responses = event(new CheckPlayerExists($player_name));
-
-        if (is_array($responses)) {
-            // @codeCoverageIgnoreStart
-            foreach ($responses as $r) {
-                if ($r) {
-                    return $next($request);
-                }
-            }
-            // @codeCoverageIgnoreEnd
-        }
-
-        if (!Player::where('name', $player_name)->get()->isEmpty()) {
-            return $next($request);
-        }
-
-        if (option('return_204_when_notfound')) {
-            return response('', 204, [
-                'Cache-Control' => 'public, max-age='.option('cache_expire_time'),
-            ]);
-        } else {
-            return abort(404, trans('general.unexistent-player'));
-        }
+        return $next($request);
     }
 }
