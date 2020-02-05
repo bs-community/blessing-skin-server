de646940026bcec90290d970f061d575c80b7b4e
Pig Fang <g-plane@hotmail.com>
2020-02-05 10:05:32 +0800

    clear timeout on unmount

--- resources/assets/src/components/Toast.tsx
+++ resources/assets/src/components/Toast.tsx
@@ -20,9 +20,15 @@ const Toast: React.FC<Props> = props => {
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => setShow(true), 100)
-    setTimeout(() => setShow(false), 3000)
-    setTimeout(props.onClose, 3100)
+    const id1 = setTimeout(() => setShow(true), 100)
+    const id2 = setTimeout(() => setShow(false), 3000)
+    const id3 = setTimeout(props.onClose, 3100)
+
+    return () => {
+      clearTimeout(id1)
+      clearTimeout(id2)
+      clearTimeout(id3)
+    }
   }, [props.onClose])
 
   const type = props.type === 'error' ? 'danger' : props.type
