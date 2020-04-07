d116b10b16221f4ae24df6f794b1fa0d70d9f6bf
Pig Fang <g-plane@hotmail.com>
2020-04-07 14:16:48 +0800

    fix loading artisan commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,16 +2,18 @@
 
 namespace App\Console;
 
+use App\Console\Commands;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
         \Laravel\Passport\Console\KeysCommand::class,
+        Commands\BsInstallCommand::class,
+        Commands\OptionsCacheCommand::class,
+        Commands\PluginDisableCommand::class,
+        Commands\PluginEnableCommand::class,
+        Commands\SaltRandomCommand::class,
+        Commands\UpdateCommand::class,
     ];
-
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-    }
 }
