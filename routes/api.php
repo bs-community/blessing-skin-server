7c6bfd627180d320190fb5969e9a4ec6405e2e10
Pig Fang <g-plane@hotmail.com>
2020-05-01 08:34:47 +0800

    add API for players management

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,15 @@ Route::prefix('closet')->middleware('auth:jwt,oauth')->group(function () {
     Route::put('{tid}', 'ClosetController@rename');
     Route::delete('{tid}', 'ClosetController@remove');
 });
+
+Route::prefix('admin')
+    ->middleware(['auth:jwt,oauth', 'role:admin'])
+    ->group(function () {
+        Route::prefix('players')->group(function () {
+            Route::get('', 'PlayersManagementController@list');
+            Route::put('{player}/name', 'PlayersManagementController@name');
+            Route::put('{player}/owner', 'PlayersManagementController@owner');
+            Route::put('{player}/textures', 'PlayersManagementController@texture');
+            Route::delete('{player}', 'PlayersManagementController@delete');
+        });
+    });
