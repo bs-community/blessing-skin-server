160dbff8349bd2d86ce6a3f3de8d789449e51941
Pig Fang <g-plane@hotmail.com>
2020-06-05 23:25:59 +0800

    fix blade template

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ locale }}">
+<html lang="{{ $locale }}">
 <head>
     @include('shared.head')
     <title>@yield('title') - {{ option_localized('site_name') }}</title>
