7bcee679c7b3618ac666dff3eb21e6772b9ce3e8
Pig Fang <g-plane@hotmail.com>
2020-06-07 23:41:15 +0800

    fix tsc error

--- resources/assets/src/index.tsx
+++ resources/assets/src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import './scripts/app'
 import routes from './scripts/route'
-import * as emitter from './scripts/event'
 
 loadModules()
 
