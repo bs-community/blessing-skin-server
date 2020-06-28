017db1788ba146c9da8c4fb8844c5eb66e559984
Pig Fang <g-plane@hotmail.com>
2020-06-28 15:57:46 +0800

    add migration

---
+++ database/migrations/2020_06_28_155519_add_user_locale_field.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserLocaleField extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('locale')->nullable()->after('nickname');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->removeColumn('locale');
+        });
+    }
+}
