a26ddfbf1917c48334e79e4724b4e4464c2b6a32
Pig Fang <g-plane@hotmail.com>
2020-07-01 15:33:50 +0800

    allow to customize fallback locale

--- config/app.php
+++ config/app.php
@@ -116,7 +116,7 @@ return [
     |
     */
 
-    'fallback_locale' => 'en',
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     /*
     |--------------------------------------------------------------------------
