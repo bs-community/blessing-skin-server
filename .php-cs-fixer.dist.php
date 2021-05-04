5550b877dfd41c3e83592b492485f8fa5adbb933
Pig Fang <g-plane@hotmail.com>
2021-05-04 18:17:01 +0800

    add php-cs-fixer as dev dependency

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,18 +4,15 @@ $finder = PhpCsFixer\Finder::create()
     ->in('app')
     ->in('database')
     ->in('routes')
-    ->in('tests')
-;
+    ->in('tests');
 
-return PhpCsFixer\Config::create()
-    ->setRules([
+$config = new PhpCsFixer\Config();
+return $config->setRules([
         '@Symfony' => true,
         'align_multiline_comment' => true,
         'array_syntax' => ['syntax' => 'short'],
         'increment_style' => ['style' => 'post'],
         'list_syntax' => ['syntax' => 'short'],
         'yoda_style' => false,
-
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
