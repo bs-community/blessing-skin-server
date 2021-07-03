12801e5f04c2bb0678d364ff38bd8cb1a137a1aa
Pig Fang <g-plane@hotmail.com>
2021-07-03 15:42:21 +0800

    Bump version to 6.0.0-beta.3

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     | Version of Blessing Skin Server.
     |
     */
-    'version' => '6.0.0-beta.2',
+    'version' => '6.0.0-beta.3',
 
     /*
     |--------------------------------------------------------------------------
