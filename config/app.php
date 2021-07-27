1f24e75f76489c6d75245934876446b00cfab527
Asnxthaony <asnxthaony@outlook.com>
2021-07-27 08:06:13 +0800

    added missing service provider

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\PluginServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
