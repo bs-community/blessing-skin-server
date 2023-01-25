0b516d02c530107c4394eeb2878a2113ef6a08d4
Pig Fang <g-plane@hotmail.com>
2023-01-25 11:48:46 +0800

    clean up

--- resources/assets/src/scripts/meta.ts
+++ resources/assets/src/scripts/meta.ts
@@ -2,8 +2,4 @@ const blessingElement = document.querySelector('#blessing-globals')!
 // @ts-ignore
 window.blessing = JSON.parse(blessingElement.textContent!)
 
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/sw.js?v6')
-})
-
 export {}
