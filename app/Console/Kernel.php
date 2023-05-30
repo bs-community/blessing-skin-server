d96dff1144f02b1397ddba361bda9be8f29ab1f3
Asnxthaony <asnxthaony@outlook.com>
2023-05-30 15:09:39 +0800

    revert auto load Console commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,11 +6,13 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Register the commands for the application.
-     */
-    protected function commands(): void
-    {
-        $this->load(__DIR__.'/Commands');
-    }
+    protected $commands = [
+        \Laravel\Passport\Console\KeysCommand::class,
+        Commands\BsInstallCommand::class,
+        Commands\OptionsCacheCommand::class,
+        Commands\PluginDisableCommand::class,
+        Commands\PluginEnableCommand::class,
+        Commands\SaltRandomCommand::class,
+        Commands\UpdateCommand::class,
+    ];
 }
