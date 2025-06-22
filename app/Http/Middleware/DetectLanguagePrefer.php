16474fb5d06f018c3b41f2742370a6f62cdd3048
Steven Qiu <tnqzh123@littlesk.in>
2025-06-22 17:48:48 +0800

    Remove locale cookie for API requests (#660)
    * Do not set locale cookie for API requests
    
    https://t.me/blessing_skin/184899
    
    * Remove redundant code

--- app/Http/Middleware/DetectLanguagePrefer.php
+++ app/Http/Middleware/DetectLanguagePrefer.php
@@ -28,7 +28,9 @@ class DetectLanguagePrefer
 
         /** @var Response */
         $response = $next($request);
-        $response->cookie('locale', $locale, 120);
+        if (!in_array('api', $request->route()->middleware())) {
+            $response->cookie('locale', $locale, 120);
+        }
 
         return $response;
     }
