33055ecbf97bdef8c5798f48bbfe97535f182b28
SANYE-YA <122528746+SANYE-CH@users.noreply.github.com>
2025-08-07 05:08:13 +0800

    fix avatar (refactor needed) (#666)

--- app/Http/Controllers/TextureController.php
+++ app/Http/Controllers/TextureController.php
@@ -167,7 +167,8 @@ class TextureController extends Controller
 
                 $lastModified = Carbon::createFromTimestamp($disk->lastModified($hash));
 
-                return Image::make($image)
+                // TODO: refactor
+                return \Intervention\Image\ImageManagerStatic::configure(['driver' => 'gd'])->make($image)
                     ->resize($size, $size)
                     ->response($usePNG ? 'png' : 'webp', 100)
                     ->setLastModified($lastModified);
